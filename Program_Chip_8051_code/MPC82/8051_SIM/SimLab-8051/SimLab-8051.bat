19306a27b53d30875d9ec1a0320b556e18798e79
skyhigh8591 <64904057+skyhigh8591@users.noreply.github.com>
2020-09-09 10:13:31 +0800

    SortOut_01

--- Program_Chip_8051_code/MPC82/8051_SIM/SimLab-8051/SimLab-8051.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start SimLab_8051.exe
--- Program_Chip_HT66_code/Source/MyType.h
+++
@@ -1,6 +0,0 @@
-typedef	unsigned char	u8;
-typedef	char			s8;
-typedef	unsigned short	u16;
-typedef	short			s16;
-typedef	unsigned long	u32;
-typedef	long			s32;
